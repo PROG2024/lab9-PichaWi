5e6bb7f31a2ffabe54ddd227c155b187937c9ea6
Picha Wiwattanawongsa <picha.w@ku.th>
2024-03-12 19:33:41 +0700

    Add test to two file

--- circle.py
+++ circle.py
@@ -9,6 +9,10 @@ class Circle:
         
         :param radius: radius of the circle, may be zero.
         :raises ValueError: if radius is negative.
+
+        >>> circle = Circle(3)
+        >>> circle.radius
+        3
         """
         if radius < 0:
             raise ValueError("radius must be non-negative")
@@ -19,6 +23,12 @@ class Circle:
         area of this circle and another circle.
         Since area is pi*r**2, the radii of the 3 circles
         should form a Pythagorean triple (r1^2 + r2^2 = r3^2)
+
+        >>> circle1 = Circle(3)
+        >>> circle2 = Circle(4)
+        >>> result = circle1.add_area(circle2)
+        >>> result.radius
+        5
         """
         r1 = self.get_radius()
         r2 = circle.get_radius()
@@ -39,3 +49,6 @@ class Circle:
         return f"Circle({self.radius})"
     
     __repr__ = __str__
+
+
+
--- test_circle.py
+++ test_circle.py
@@ -10,3 +10,22 @@ Write unit tests as described in README.md.
 from circle import Circle
 
 # TODO write 3 tests as described above
+
+
+class TestCircle:
+
+    def test_add_area(self):
+        C1 = Circle(3)
+        C2 = Circle(4)
+        result = C1.add_area(C2)
+        assert result.get_radius() == 5
+
+    def test_add_area_edge_case(self):
+        Circle1 = Circle(0)
+        Circle2 = Circle(5)
+        result2 = Circle1.add_area(Circle2)
+        assert result2.get_radius() == 5
+
+    def test_add_area_negative_case(self):
+        with self.assertRaises(ValueError):
+            Circle(-1)
